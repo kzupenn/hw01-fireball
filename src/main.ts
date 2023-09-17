3ce40333be0823ba76e1429f8c60785e7340bb60
kevinz2003 <62074534+kevinz2003@users.noreply.github.com>
2023-09-17 19:57:43 -0400

    audio

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import {vec2, vec3} from 'gl-matrix';
-// import * as Stats from 'stats-js';
-// import * as DAT from 'dat-gui';
+import {vec3, vec4} from 'gl-matrix';
+const Stats = require('stats-js');
+import * as DAT from 'dat.gui';
+import Icosphere from './geometry/Icosphere';
 import Square from './geometry/Square';
+import Cube from './geometry/Cube';
 import OpenGLRenderer from './rendering/gl/OpenGLRenderer';
 import Camera from './Camera';
 import {setGL} from './globals';
@@ -12,45 +14,107 @@ import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
 const controls = {
   tesselations: 5,
   'Load Scene': loadScene, // A function pointer, essentially
+  red: 256,
+  green: 0,
+  blue: 0,
+  'Toggle Frag': toggleFrag,
+  'Toggle Vert': toggleVert
 };
 
+let icosphere: Icosphere;
 let square: Square;
-let time: number = 0;
+let cube: Cube;
+let prevTesselations: number = 5;
+let prevFragVert = {frag: 0, vert:0};
 
 function loadScene() {
+  icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, controls.tesselations);
+  icosphere.create();
   square = new Square(vec3.fromValues(0, 0, 0));
   square.create();
-  // time = 0;
+  cube = new Cube(vec3.fromValues(0, 0, 0), 1);
+  cube.create();
 }
 
+const fragvert = {
+  frag: 0,
+  vert: 0
+}
+function toggleFrag() {
+  if(fragvert.frag == 0){
+    fragvert.frag = 1;
+  }
+  else {
+    fragvert.frag = 0;
+  }
+}
+
+function toggleVert() {
+  if(fragvert.vert == 0){
+    fragvert.vert = 1;
+  }
+  else {
+    fragvert.vert = 0;
+  }
+}
+
+
+//Audio
+var auctx = new(window.AudioContext)();
+var buffer, src, analyser, buffLen;
+var barWidth, dataArray;
+
+function startAudio() {
+  var url = "https://www.youtube.com/watch?v=-xGDNVejJlg&list=RDluc5N6Pf3rE&index=3";
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function() {
+    auctx.decodeAudioData(request.response, function(buffer) {
+      buffer = buffer;
+      src = auctx.createBufferSource();
+      src.buffer = buffer;
+      src.loop = false;
+      src.connect(auctx.destination);
+      src.start(0);
+      analyser = auctx.createAnalyser();
+      src.connect(analyser);
+      analyser.connect(auctx.destination);
+      analyser.fftSize = 256;
+      buffLen = analyser.frequencyBinCount;
+      dataArray = new Uint8Array(buffLen);
+      barWidth = (500 - 2 * buffLen - 4) / buffLen * 2.5;
+      // ctx.lineWidth = barWidth;
+      // draw();
+    });
+  }
+  request.send();
+}
+startAudio();
+
 function main() {
-  window.addEventListener('keypress', function (e) {
-    // console.log(e.key);
-    switch(e.key) {
-      // Use this if you wish
-    }
-  }, false);
-
-  window.addEventListener('keyup', function (e) {
-    switch(e.key) {
-      // Use this if you wish
-    }
-  }, false);
-
   // Initial display for framerate
-  // const stats = Stats();
-  // stats.setMode(0);
-  // stats.domElement.style.position = 'absolute';
-  // stats.domElement.style.left = '0px';
-  // stats.domElement.style.top = '0px';
-  // document.body.appendChild(stats.domElement);
+  const stats = Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild(stats.domElement);
 
   // Add controls to the gui
-  // const gui = new DAT.GUI();
+  const gui = new DAT.GUI();
+  gui.add(controls, 'tesselations', 0, 8).step(1);
+  gui.add(controls, 'Load Scene');
+  gui.add(controls, 'red', 0, 256).step(1);
+  gui.add(controls, 'green', 0, 256).step(1);
+  gui.add(controls, 'blue', 0, 256).step(1);
+  gui.add(controls, 'Toggle Frag');
+  gui.add(controls, 'Toggle Vert');
+
 
   // get canvas and webgl context
   const canvas = <HTMLCanvasElement> document.getElementById('canvas');
-  const gl = <WebGL2RenderingContext> canvas.getContext('webgl2');
+  const gl = <WebGL2RenderingContext> canvas.getContext('webgl2', {alpha:false});
   if (!gl) {
     alert('WebGL 2 not supported!');
   }
@@ -61,49 +125,68 @@ function main() {
   // Initial call to load scene
   loadScene();
 
-  const camera = new Camera(vec3.fromValues(0, 0, -10), vec3.fromValues(0, 0, 0));
+  const camera = new Camera(vec3.fromValues(0, 0, 5), vec3.fromValues(0, 0, 0));
 
   const renderer = new OpenGLRenderer(canvas);
-  renderer.setClearColor(164.0 / 255.0, 233.0 / 255.0, 1.0, 1);
+  renderer.setClearColor(0.2, 0.2, 0.2, 1);
   gl.enable(gl.DEPTH_TEST);
 
-  const flat = new ShaderProgram([
-    new Shader(gl.VERTEX_SHADER, require('./shaders/flat-vert.glsl')),
-    new Shader(gl.FRAGMENT_SHADER, require('./shaders/flat-frag.glsl')),
+  const frags = [
+    new Shader(gl.FRAGMENT_SHADER, require('./shaders/lambert-frag.glsl')), 
+    new Shader(gl.FRAGMENT_SHADER, require('./shaders/perlin-frag.glsl'))
+  ];
+  const verts = [
+    new Shader(gl.VERTEX_SHADER, require('./shaders/lambert-vert.glsl')), 
+    new Shader(gl.VERTEX_SHADER, require('./shaders/trig-vert.glsl'))
+  ];
+
+  var shaderprog = new ShaderProgram([
+    verts[0],
+    frags[0]
   ]);
 
-  function processKeyPresses() {
-    // Use this if you wish
-  }
-
+  let timetick: number = 0;
   // This function will be called every frame
   function tick() {
     camera.update();
-    // stats.begin();
+    stats.begin();
     gl.viewport(0, 0, window.innerWidth, window.innerHeight);
     renderer.clear();
-    processKeyPresses();
-    renderer.render(camera, flat, [
-      square,
-    ], time);
-    time++;
-    // stats.end();
+    if(controls.tesselations != prevTesselations)
+    {
+      prevTesselations = controls.tesselations;
+      icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, prevTesselations);
+      icosphere.create();
+    }
+    if(fragvert.frag != prevFragVert.frag || fragvert.vert != prevFragVert.vert) {
+      shaderprog = new ShaderProgram([verts[fragvert.vert],
+        frags[fragvert.frag]]);
+      prevFragVert.frag = fragvert.frag;
+      prevFragVert.vert = fragvert.vert;
+      timetick = 0;
+    }
+    
+    
+    renderer.render(camera, shaderprog, [
+      icosphere,
+      // square,
+    ], vec4.fromValues(controls.red/256, controls.green/256, controls.blue/256, 1), timetick);
+    stats.end();
 
     // Tell the browser to call `tick` again whenever it renders a new frame
     requestAnimationFrame(tick);
+    timetick++;
   }
 
   window.addEventListener('resize', function() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.setAspectRatio(window.innerWidth / window.innerHeight);
     camera.updateProjectionMatrix();
-    flat.setDimensions(window.innerWidth, window.innerHeight);
   }, false);
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.setAspectRatio(window.innerWidth / window.innerHeight);
   camera.updateProjectionMatrix();
-  flat.setDimensions(window.innerWidth, window.innerHeight);
 
   // Start the render loop
   tick();
